dbb93ce97bf3a7fcec861b1f71f118f900e4c47c
Bharatkhatri <bharatkhatri351@gmail.com>
2022-06-20 20:07:58 +0530

    dark mode added
    New File appdark.theme is added. Please review this.

--- src/Header.js
+++ src/Header.js
@@ -1,43 +1,40 @@
-import './Header.css';
+import "./Header.css";
 import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <div className="header">
-            <div className="logo">
-                <Link to="/">
-                    <img className='logoImage' src="logo512.png" alt='Logo'/>
-                </Link>
-            </div>
-            <div className='link'>
-                <Link to="/projects">
-                    <div className="header_option">
-                        <span className="projects_optionLineOne">Projects</span>
-                    </div>
-                </Link>
-                <Link to="/Resume">
-                    <div className="header_option">
-                        <span className="resume_optionLineOne">Resume</span>
-                    </div>
-                </Link>
-                <Link to="/About">
-                    <div className="header_option">
-                        <span className="about_optionLineOne">About</span>
-                    </div>
-                </Link>
-                <Link to="/Contact">
-                    <div className="header_option">
-                        <span className="contact_optionLineOne">Connect</span>
-                    </div>
-                </Link>
-                <Link to="/mode">
-                    <div className="header_option">
-                        <span className="contact_optionLineOne">Mode</span>
-                    </div>
-                </Link>
-            </div>
+  return (
+    <div className="header">
+      <div className="logo">
+        <Link to="/">
+          <img className="logoImage" src="logo512.png" alt="Logo" />
+        </Link>
+      </div>
+      <div className="link">
+        <Link to="/projects">
+          <div className="header_option">
+            <span className="projects_optionLineOne">Projects</span>
+          </div>
+        </Link>
+        <Link to="/Resume">
+          <div className="header_option">
+            <span className="resume_optionLineOne">Resume</span>
+          </div>
+        </Link>
+        <Link to="/About">
+          <div className="header_option">
+            <span className="about_optionLineOne">About</span>
+          </div>
+        </Link>
+        <Link to="/Contact">
+          <div className="header_option">
+            <span className="contact_optionLineOne">Connect</span>
+          </div>
+        </Link>
+        <div className="header_option">
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Header;
